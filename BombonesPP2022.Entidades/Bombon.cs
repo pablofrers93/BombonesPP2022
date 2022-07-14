f413b86cab10cbb18ddb87a95ac1dbc966d520c0
pablofrers93 <87342537+pablofrers93@users.noreply.github.com>
2022-07-14 01:57:12 -0300

    Add files via upload

--- BombonesPP2022.Entidades/Bombon.cs
+++ BombonesPP2022.Entidades/Bombon.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace BombonesPP2022.Entidades
 {
-    public class Bombon
+    public class Bombon:ICloneable
     {
         public int BombonId { get; set; }
         public string NombreBombon { get; set; }
         public int TipoChocolateId { get; set; }
         public int TipoNuezId { get; set; }
         public int TipoRellenoId { get; set; }
-        public double PrecioVenta { get; set; }
+        public decimal PrecioVenta { get; set; }
         public int Stock { get; set; }
         public int FabricaId { get; set; }
         public byte[] RowVersion { get; set; }
@@ -22,5 +22,11 @@ namespace BombonesPP2022.Entidades
         public Tipo_Chocolate Tipo_Chocolate { get; set; }
         public Tipo_Nuez Tipo_Nuez { get; set; }
         public Tipo_Relleno Tipo_Relleno { get; set; }
+        public Fabrica Fabrica { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
     }
 }
