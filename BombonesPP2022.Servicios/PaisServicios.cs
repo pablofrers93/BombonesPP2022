1fb829f0d9eb235178a205811b8acd7ef4f91bbc
pablofrers93 <87342537+pablofrers93@users.noreply.github.com>
2022-07-15 06:34:32 -0300

    Add files via upload

---
+++ BombonesPP2022.Servicios/PaisServicios.cs
@@ -0,0 +1,69 @@
+﻿using BombonesPP2022.Datos;
+using BombonesPP2022.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombonesPP2022.Servicios
+{
+    public class PaisServicios
+    {
+        private readonly PaisesRepositorio repositorio;
+
+        public PaisServicios()
+        {
+            repositorio = new PaisesRepositorio();
+        }
+
+        public List<Pais> GetLista()
+        {
+            try
+            {
+                return repositorio.GetLista();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+        public int Agregar(Pais pais)
+        {
+            try
+            {
+                return repositorio.Agregar(pais);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public int Borrar(Pais pais)
+        {
+            try
+            {
+                return repositorio.Borrar(pais);
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        public int Editar(Pais pais)
+        {
+            try
+            {
+                return repositorio.Editar(pais);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+    }
+}
